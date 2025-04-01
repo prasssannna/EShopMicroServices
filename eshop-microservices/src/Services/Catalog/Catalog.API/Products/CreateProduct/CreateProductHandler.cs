c03b303bf8e7be4a0128019187b12b7c6de3ab58
Prassanna Suryanarayanan <prasssannna@gmail.com>
2025-04-01 18:25:52 +0100

    Added catalog api full code

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)   : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidator : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Create Prodcut entity from command object. Refer line 5       
+        var product = new Product()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+             ImageFile = command.ImageFile,
+             Price = command.Price 
+        };
+
+        //TODO
+        //save to database
+
+        //Return the result refer line 6
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
+
--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services
 var app = builder.Build();
+//Add routiong pipeline
 
 app.MapGet("/", () => "Hello World!");
 
